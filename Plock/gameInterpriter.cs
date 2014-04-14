f5bd8273e1429504a1bca3e1a25acd27eeac5f06
taguro <tagurodevelop@yahoo.co.jp>
2014-04-14 19:46:35 +0900

    gameInterpriterの可読性の向上（コメントの追加とメソッドの場所の変更

--- Plock/gameInterpriter.cs
+++ Plock/gameInterpriter.cs
@@ -60,15 +60,66 @@ namespace Plock
             collerCode = null;
             colleeCode = null;
         }
-        public bool isEnd()
+        /// <summary>
+        /// メソッドを実行する
+        /// </summary>
+        /// <param name="game">gameにvalue（のメソッド）を実行させる</param>
+        /// <returns>実行後のgameを返す</returns>
+        internal GameData execute(GameData game)
         {
-            return _value == null;
+            return value.execute(game);
         }
 
-        void setValue(Queue<String> codeQueue){
-            
+        /// <summary>
+        /// 次に実行するべき位置のコードリストを返す
+        /// </summary>
+        /// <param name="game"></param>
+        /// <returns></returns>
+        internal CodeList getMoveTo(GameData game)
+        {
+            if (value.isControlMethod()) return getCodeList(value.getMoveTo(game));//制御文の時は要検討（制御文による）
+            if(nextCode!=null)return nextCode;//次のコードがあるときは次のコード
+            return collerCode.nextCode;//次のコードがない時は要検討（コードによる）
+        }
+
+        /// <summary>
+        /// Enum型の位置に対応するコードリストを返す
+        /// </summary>
+        /// <param name="moveTo">移動先を示すEnum型</param>
+        /// <returns>移動先のコードリスト</returns>
+        internal CodeList getCodeList(MoveTo moveTo)
+        {
+            switch (moveTo)
+            {
+                case MoveTo.NEXT:
+                    return nextCode;
+                case MoveTo.COLLERNEXT:
+                    return collerCode;
+                case MoveTo.COLLEE:
+                    return colleeCode;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 文字列を受け取って、実行できるようにメソッドを設定する
+        /// </summary>
+        /// <param name="code"></param>
+        public void setValue(String code)
+        {
+            var codeQueue = splitToQueue(code);//前処理
+            setValue(codeQueue);//移譲
+        }
+
+        /// <summary>
+        /// キューに格納された文字列を受け取って、実行できるようにメソッドを設定する
+        /// </summary>
+        /// <param name="codeQueue"></param>
+        void setValue(Queue<String> codeQueue)
+        {
+
             if (codeQueue.Count == 0) return;
-            
+
             _value = codeQueue.Dequeue();
             value.set(_value);
 
@@ -100,12 +151,6 @@ namespace Plock
             }
         }
 
-        public void setValue(String code)
-        {
-            var codeQueue = splitToQueue(code);
-            setValue(codeQueue);
-        }
-
         /// <summary>
         /// コードを/r/nごとに区切って、順次キューに格納して返す
         /// /// </summary>
@@ -123,30 +168,13 @@ namespace Plock
             return codeQueue;
         }
 
-        internal GameData execute(GameData game)
+        /// <summary>
+        /// 一番最後のコード（の次）ならtrueを返す
+        /// </summary>
+        /// <returns></returns>
+        public bool isEnd()
         {
-            return value.execute(game);
-        }
-
-        internal CodeList getMoveTo(GameData game)
-        {
-            if (value.isControlMethod()) return getCodeList(value.getMoveTo(game));//制御文の時は要検討（制御文による）
-            if(nextCode!=null)return nextCode;//次のコードがあるときは次のコード
-            return collerCode.nextCode;//次のコードがない時は要検討（コードによる）
-        }
-
-        internal CodeList getCodeList(MoveTo moveTo)
-        {
-            switch (moveTo)
-            {
-                case MoveTo.NEXT:
-                    return nextCode;
-                case MoveTo.COLLERNEXT:
-                    return collerCode;
-                case MoveTo.COLLEE:
-                    return colleeCode;
-            }
-            return null;
+            return _value == null;
         }
     }
 
